454b700a8496505804abbbba2c97ec35cd6b8376
Md Nazim Uddin <isnamdnazim@gmail.com>
2022-09-29 00:55:33 +0600

    User Registration is working fine

---
+++ Controllers/UserController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+using VerifyEmailForgotPassword.Data;
+using VerifyEmailForgotPassword.Models;
+
+namespace VerifyEmailForgotPassword.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public readonly DataContext _dataContext;
+        public UserController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserRegisterRequest request)
+        {
+            if(_dataContext.Users.Any(u => u.Email == request.Email))
+            {
+                return BadRequest("User Already Exists");
+            }
+
+            CreatePasswordHash(request.Password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            var user = new User
+            {
+                Email = request.Email,
+                PasswordHash = passwordHash,
+                PasswordSalt = passwordSalt,
+                VerificationToken = CreateRandomToken()
+            };
+
+            _dataContext.Users.Add(user);
+            await _dataContext.SaveChangesAsync();
+
+            return Ok(user);
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        private string CreateRandomToken()
+        {
+            return Convert.ToHexString(RandomNumberGenerator.GetBytes(64));
+        }
+    }
+}
